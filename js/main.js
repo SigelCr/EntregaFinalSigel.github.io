ce874384fbc3eeb68078bfd732442199e32ffff0
tomas sigel <tomasmsigel@hotmail.com>
2023-02-23 15:37:59 -0300

    proyecto 50%

--- js/main.js
+++ js/main.js
@@ -1,122 +1,240 @@
-function bucleDelPrompt() {
+let botonMostrar = document.getElementById("botonMostrar");
+let botonComprar = document.getElementById("botonComprar");
+let verdad = true;
 
-let productos = [ 
-  { id: 1, nombre: "remera corta", categoria: "remeras", precio: 2500 },
-  { id: 2, nombre: "remera larga", categoria: "remeras", precio: 2200 },
-  { id: 3, nombre: "remera sin manga", categoria: "remeras", precio: 2000 },
-  { id: 4, nombre: "camisa floreada", categoria: "remeras", precio: 3000 },
+const productos = [ 
+      { id: 1, nombre: "Simple", categoria: "hamburguesas", precio: 1000 },
+      { id: 2, nombre: "Con queso", categoria: "hamburguesas", precio: 1200 },
+      { id: 3, nombre: "Triple queso", categoria: "hamburguesas", precio: 2000 },
+      { id: 4, nombre: "Big", categoria: "hamburguesas", precio: 2100 },
+      { id: 5, nombre: "Triple tasty", categoria: "hamburguesas", precio: 2300 },
+      { id: 6, nombre: "Doble Bacon", categoria: "hamburguesas", precio: 2100 },
+      { id: 7, nombre: "Doble cheddar", categoria: "hamburguesas", precio: 2500 },
+      { id: 8, nombre: "Triple Cheddar", categoria: "hamburguesas", precio: 3000 },
+    ];
 
-  { id: 5, nombre: "bermuda", categoria: "pantalones", precio: 1200 },
-  { id: 6, nombre: "short", categoria: "pantalones", precio: 1000 },
-  { id: 7, nombre: "jean", categoria: "pantalones", precio: 2000 },
-  { id: 8, nombre: "chupin", categoria: "pantalones", precio: 1800 },
 
-  { id: 9, nombre: "cadena", categoria: "bijouteries", precio: 800 },
-  { id: 10, nombre: "arito", categoria: "bijouteries", precio: 500 },
-  { id: 11, nombre: "pulsera", categoria: "bijouteries", precio: 500 },
-  { id: 12, nombre: "anillo", categoria: "bijouteries", precio: 200 },
-
-  { id: 13, nombre: "mochila", categoria: "accesorios", precio: 2500 },
-  { id: 14, nombre: "cartera", categoria: "accesorios", precio: 3000 } ,
-  { id: 15, nombre: "pañuelo", categoria: "accesorios", precio: 1500 },
-  { id: 16, nombre: "billetera", categoria: "accesorios", precio: 1200 },
+const imagenes = [
+  `../imagenes/hamburguesa1.png`,
+  `../imagenes/hamburguesa2.png`,
+  `../imagenes/hamburguesa3.png`,
+  `../imagenes/hamburguesa4.png`,
+  `../imagenes/hamburguesa5.png`,
+  `../imagenes/hamburguesa6.png`,
+  `../imagenes/hamburguesa7.png`,
+  `../imagenes/hamburguesa8.png`,
 ];
-let filtrados;
-
-let filtro = prompt(`elija el filtro a buscar: \n- Categoria \n- Precio \n- Id \n- Salir`);
-
-if (filtro === "precio") {
-  let precio = parseInt(prompt("Ingrese el precio mínimo a buscar"));
-  
-  filtrados = productos.filter((item) => item.precio > precio);
-
-}else if (filtro === "categoria") {
-  let categoria = prompt(`eliga la categoria que esta buscando:\n- remeras \n- pantalones \n- bijouteries \n- accesorios`);
-
-  filtrados = productos.filter((item) => item.categoria === categoria);
-
-}else if(filtro === "id"){
-  alert(`Actualmente tenemos 16 ID`) // aca queria hacer como un bucle para que muestre todos los id pero no pude, sino como sabe el usuario el id de cada producto? ah no ser que lo este viendo en pantalla
-  let id = parseInt(prompt("ingrese el id del producto que desea ver"));
-
-  filtrados = productos.filter((item) => item.id === id);
-
-}else if (filtro === "salir") {
-  alert("Saliendo...")
-}
-
-else {
-  alert(`El filtro ${filtro} no se encuentra disponible, por favor escriba los que estan en la lista`);
-    while (filtro !== "precio" && filtro !== "categoria" && filtro !== "id") {
-      bucleDelPrompt() 
-      break; //creo que no funciona lo dejo x las dudas
-  };
-  
-}
-
-if (filtrados.length > 0) { // si fuese numero negativo es false
-  let mensaje; // para que los productos aparezcan juntos en vez de 1x1
-  filtrados.forEach((item) => {
-    mensaje = // para que los productos aparezcan juntos en vez de 1x1
-    ` 
-     Id: ${item.id}
-     Nombre: ${item.nombre}
-     Precio: ${item.precio}
-     `;
-    alert(mensaje);
-  });
-} else {
-  alert("No se encontraron productos");
-}
+let contadorImagenes = 0;
 
 
+//boton ver//
 
-//carrito
-function comprarProductos() {
-let idProducto = Number(prompt("Escriba el ID del producto que desea agregar al carrito"));
 
-let carrito = [];
+function verProductos(){
+    
+    botonMostrar.className = "boton"
+    
+    productos.forEach(item => {
+      let img = document.createElement("img")
+      img.src = imagenes[contadorImagenes]
+      contadorImagenes = (contadorImagenes + 1) % imagenes.length;
+      img.className = "imagen-btn";
+        
+      let div = document.createElement("div")
+      div.innerHTML = `<p><b class="tamaño-titulo">${item.nombre}</b></p>`;
 
-let productoSeleccionado = productos.find(productos => productos.id === idProducto); 
-
-if (productoSeleccionado){
-  function agregarAlCarrito(productos) {
-    carrito.push(productos);
-    alert(`el producto ${productos.nombre} por $${productos.precio} ha sido agregado al carrito exitosamente`)
-  }
-  agregarAlCarrito(productoSeleccionado); // invocacion
-}else {
-  alert(`Recuerde tipear el ID de los productos que se encuentran disponibles`);
-}
-
-let verProductosCarrito = prompt("¿quiere ver los productos agregados al carrito? (si/no)");
-if(verProductosCarrito === "si"){
-  function verCarrito(carrito){
-    carrito.forEach(function(productos){ //adentro del forEach hay una funcion producto que contiene el alert
-      alert(`Productos agregados al carrito: ${productos.nombre} $${productos.precio}`)
-    });
-    let agregarOtroProducto = prompt("¿quiere seguir agregando productos al carrito? (si/no)");
-      switch (agregarOtroProducto) {
-        case "si":
-          comprarProductos(); // no se como hacer para que la primera compra se guarde, y cuando agregue otro producto, se muestren ambos
-
-          break;
-        case "no":
-          alert("Muchas gracias por su compra");
-          
-          break;
+      let div2 = document.createElement("div")
+      div2.innerHTML = `<p>Precio: $${item.precio}</p><br><br><hr>`;
       
+      botonMostrar.append(img);
+      botonMostrar.append(div);
+      botonMostrar.append(div2);
+      
+    })
+}
+
+
+function abrirCerrarBotonProductos(){
+  if(verdad){
+    verProductos();
+    console.log("abrir menu");
+    verdad = false;
+  }
+  else {
+    botonMostrar.innerHTML = `Nuestras hamburguesas`;
+    console.log("cerrar menu");
+    verdad = true;
+  }
+}
+
+botonMostrar.addEventListener("click", abrirCerrarBotonProductos);
+
+
+
+//boton comprar//
+
+
+
+function comprarProductos(){
+  let parrafo = document.createElement("p");
+  parrafo.innerHTML = `<p class="parrafo">Escriba el nombre de la hamburguesa que desea comprar</p>`;
+
+  let input = document.createElement("input");
+  input.className = "input";
+  input.setAttribute("placeholder","Ejemplo: doble cheddar")
+
+  let inputSubmit = document.createElement("input");
+  inputSubmit.type = "submit";
+  inputSubmit.className = "botonInput";
+  inputSubmit.value = "Agregar al carrito";
+
+
+
+
+  inputSubmit.addEventListener("click", () => {
+    const productoBuscado = input.value.toLowerCase(); //devuelve el valor escrito en el input
+    let contenedorJs;
+      switch (productoBuscado) {
+        case "simple":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa simple ha sido agregada</h2>
+            <p>$1000</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+
+            botonComprar.append(contenedorJs)
+          break;
+        case "con queso":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Con queso ha sido agregada</h2>
+            <p>$1200</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
+        case "triple queso":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Triple queso ha sido agregada</h2>
+            <p>$2000</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
+        case "big":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Big ha sido agregada</h2>
+            <p>$2100</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
+        case "triple tasty":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Triple tasty ha sido agregada</h2>
+            <p>$2300</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
+        case "doble bacon":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Doble bacon ha sido agregada</h2>
+            <p>$2100</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
+        case "doble cheddar":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Doble cheddar ha sido agregada</h2>
+            <p>$2500</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
+        case "triple cheddar":
+            contenedorJs = document.createElement("div");
+            contenedorJs.className = "contenedorJs";
+            contenedorJs.innerHTML = `
+            <h1>Bienvenido al carrito</h1>
+            <h2>Hamburguesa Triple cheddar ha sido agregada</h2>
+            <p>$3000</p>
+            <button type="text" class="btn-emergente">Comprar</button>
+            `
+            botonComprar.append(contenedorJs)
+            console.log("funciona");
+          break;
         default:
-            alert(`Por favor recuerde responder con "si" o "no" e intentelo de nuevo`);
+          alert("Escriba bien los nombres de los productos, gracias");
+          console.log("no funciona");
+
           break;
       }
-  }
-  verCarrito(carrito); // invocacion
 
-}else {
-  alert("Gracias por su compra, vuelva pronto")
+  })
+
+  inputSubmit.addEventListener("click", (evento) => { //para que no se cierre el botonComprar cuando hago click en buscar.
+    evento.stopPropagation();
+  })
+
+  input.addEventListener("click", (evento) => { //para que no se cierre el botonComprar cuando hago click en el input.
+    evento.stopPropagation();
+  })
+
+  input.addEventListener("keyup", (e) => { //para que al presionar y soltar el espacio desde la pc no se me cierre el botonComprar.
+    if(e.key === " "){
+      console.log("espacio");
+      e.preventDefault(); //evita el comportamiento x defecto que hace la tecla o los formularios o lo sea. en este caso la tecla "espacio".
+    }
+  });
+
+  botonComprar.append(parrafo);
+  botonComprar.append(input);
+  botonComprar.append(inputSubmit);
 }
+
+
+
+function abrirCerrarBotonComprar(){
+  if(verdad){
+    console.log("abrir comprar");
+    comprarProductos();
+    verdad = false;
+  }
+  else {
+    console.log("cerrar comprar");
+    botonComprar.innerHTML = `<span class="boton">Comprar</span>`;
+    verdad = true;
+  }
 }
-comprarProductos();
-}
-//bucleDelPrompt();
+
+botonComprar.addEventListener("click", abrirCerrarBotonComprar);
+
+

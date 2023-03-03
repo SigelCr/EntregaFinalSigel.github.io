f8722e699463356740b5fe86bbfcf952593ee1e5
tomas sigel <tomasmsigel@hotmail.com>
2023-03-03 18:44:15 -0300

    proyecto 60%

--- js/main.js
+++ js/main.js
@@ -1,59 +1,180 @@
 let botonMostrar = document.getElementById("botonMostrar");
-let botonComprar = document.getElementById("botonComprar");
+let botonCarrito = document.getElementById("botonCarrito");
 let verdad = true;
+let carritoVacio = JSON.parse(localStorage.getItem("carrito")) || [];
+let carrito = document.getElementById(`carrito`)
 
 const productos = [ 
-      { id: 1, nombre: "Simple", categoria: "hamburguesas", precio: 1000 },
-      { id: 2, nombre: "Con queso", categoria: "hamburguesas", precio: 1200 },
-      { id: 3, nombre: "Triple queso", categoria: "hamburguesas", precio: 2000 },
-      { id: 4, nombre: "Big", categoria: "hamburguesas", precio: 2100 },
-      { id: 5, nombre: "Triple tasty", categoria: "hamburguesas", precio: 2300 },
-      { id: 6, nombre: "Doble Bacon", categoria: "hamburguesas", precio: 2100 },
-      { id: 7, nombre: "Doble cheddar", categoria: "hamburguesas", precio: 2500 },
-      { id: 8, nombre: "Triple Cheddar", categoria: "hamburguesas", precio: 3000 },
+      { id: 1, nombre: "Simple",            precio: 1000, imagen: `./imagenes/hamburguesa1.png`, descripcion: "El sabor de la carne 100% vacuna más deliciosa, acompañado del pan más esponjoso, kétchup, mostaza y cebolla triturada."},
+      { id: 2, nombre: "Doble",             precio: 1200, imagen: `./imagenes/hamburguesa2.png`, descripcion: "Hamburguesa elaborada con dos medallones de carne 100% de carne vacuna y dos fetas de nuestro exclusivo Quedo Cheddar."},
+      { id: 3, nombre: "Triple",            precio: 2000, imagen: './imagenes/hamburguesa3.png', descripcion: "Tres medallones de carne 100% vacuna, queso derretido, mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar."},
+      { id: 4, nombre: "Big",               precio: 2100, imagen: `./imagenes/hamburguesa4.png`, descripcion: "Dos hamburguesas de carne con salsa especial y queso derretido, cebolla, lechuga y pepino, la hace la hamburguesa más famosa del mundo."},
+      { id: 5, nombre: "Triple Tasty",      precio: 2300, imagen: `./imagenes/hamburguesa5.png`, descripcion: "Tres medallones de carne, tres fetas de Queso Cheddar, cebolla, lechuga y tomate frescos. Sumado Salsa Tasty."},
+      { id: 6, nombre: "Doble Bacon",       precio: 2100, imagen: `./imagenes/hamburguesa6.png`, descripcion: "Dos carnes, inigualable queso cheddar, cebolla, kétchup y mostaza, y el increíble ingrediente que lo hace único: bacon premium."},
+      { id: 7, nombre: "Tasty Doble Bacon", precio: 2500, imagen: `./imagenes/hamburguesa7.png`, descripcion: "Grand Tasty Turbo Bacon Doble"},
+      { id: 8, nombre: "Tasty Triple Bacon",precio: 3000, imagen: `./imagenes/hamburguesa8.png`, descripcion: "Al tradicional sabor inconfundible de la salsa Tasty ahora lo potenciamos con nuestro espectacular Bacon crujiente y con nuestro característico Bacon Crispy triturado."},
     ];
 
+localStorage.setItem("productos",JSON.stringify(productos));
 
-const imagenes = [
-  `../imagenes/hamburguesa1.png`,
-  `../imagenes/hamburguesa2.png`,
-  `../imagenes/hamburguesa3.png`,
-  `../imagenes/hamburguesa4.png`,
-  `../imagenes/hamburguesa5.png`,
-  `../imagenes/hamburguesa6.png`,
-  `../imagenes/hamburguesa7.png`,
-  `../imagenes/hamburguesa8.png`,
-];
-let contadorImagenes = 0;
-
-
-//boton ver//
-
-
+//boton ver productos
 function verProductos(){
+  
+  botonMostrar.className = "boton";
     
-    botonMostrar.className = "boton"
-    
-    productos.forEach(item => {
-      let img = document.createElement("img")
-      img.src = imagenes[contadorImagenes]
-      contadorImagenes = (contadorImagenes + 1) % imagenes.length;
-      img.className = "imagen-btn";
-        
-      let div = document.createElement("div")
-      div.innerHTML = `<p><b class="tamaño-titulo">${item.nombre}</b></p>`;
+  productos.forEach(item => {
+    let contenedorCarta = document.createElement("div");
+    contenedorCarta.innerHTML = `
+    <div>
+      <img class="imagen-btn" src="${item.imagen}" alt="Card image cap">
+      <div>
+          <h3>${item.nombre}</h3>
+          <p>$${item.precio}</p>
+          <h5 class="descripcion">${item.descripcion}</h5>
+          <button class="boton" id="${item.id}">Añadir al carrito</button> <br><br><hr>
+      </div>
+    </div>
+    `;                               // item.id como id del boton para que reconozca a cual producto le estoy agregando al carro, sino serian todos los botones iguales
+    botonMostrar.append(contenedorCarta);
 
-      let div2 = document.createElement("div")
-      div2.innerHTML = `<p>Precio: $${item.precio}</p><br><br><hr>`;
-      
-      botonMostrar.append(img);
-      botonMostrar.append(div);
-      botonMostrar.append(div2);
-      
+    let botonAñadirCarrito = document.getElementById(`${item.id}`) //lo cree en el innerHTML de contenedorcarta y ahora me traigo el id
+
+    botonAñadirCarrito.addEventListener("click", (e) => {
+      e.stopPropagation(); // para que no se cierre el boton "nuestras hamburguesas" al hacer click en el btn de agregar al carrito
     })
+
+    botonAñadirCarrito.addEventListener("click", () => guardarEnCarrito(item.id))
+  })
+
+//Guardar en el carrito (funcion dentro del forEach)
+const guardarEnCarrito = (id) => {
+  let productoEncontrado = productos.find(item => item.id === id)
+
+  let productoYaEnCarrito = carritoVacio.find(item => item.id === id);
+
+  if(productoYaEnCarrito){
+    Swal.fire({
+      title: `Ya está añadida al carrito :)`,
+      width: `300px`,
+      timer: 1500,
+      timerProgressBar: true,
+      backdrop: false,
+      showConfirmButton: false,
+    })
+  }
+  else {
+    carritoVacio.push(productoEncontrado);
+    localStorage.setItem("carrito", JSON.stringify(carritoVacio));
+    console.log(carritoVacio);
+    Swal.fire({
+      title: `Hamburguesa ${productoEncontrado.nombre} añadida al carrito :)`,
+      width: `400px`,
+      timer: 1500,
+      timerProgressBar: true,
+      backdrop: false,
+      showConfirmButton: false,
+    })
+  }     
+}
 }
 
+botonMostrar.addEventListener("click", abrirCerrarBotonProductos);
 
+//boton ver carrito//
+function verCarrito(){
+  
+  carrito.innerHTML = ``;
+
+  carritoVacio.forEach(item => {
+    let contenedorCarrito = document.createElement(`div`);
+    contenedorCarrito.innerHTML = `
+    <div class="contenedorCarrito ${item.id}">
+      <br>
+      <h1>Hamburguesa ${item.nombre} lista para llevar</h1>
+      <img class="imagen-btn" src="${item.imagen}" alt="hamburguesa">
+      <div>
+          <h5>Nombre: ${item.nombre}</h5>
+          <p>Precio: $${item.precio}</p>
+          <input id="${item.id}" type="submit" value="deshacer"></input>
+      </div>
+    </div>
+    `;
+    botonCarrito.append(contenedorCarrito);
+    
+    let inputDeshacer = document.getElementById(`${item.id}`);
+
+    inputDeshacer.addEventListener("click", (e) => {
+      e.stopPropagation();
+    })
+
+    inputDeshacer.addEventListener("click", () => {
+      botonDeshacer(item.id)
+    })
+    
+//Funcion para sacar el producto del carrito
+function botonDeshacer(id){
+
+  let indice = carritoVacio.findIndex(producto => producto.id === id);
+  
+  if(indice !== -1) { 
+    carritoVacio.splice(indice, 1);
+    Swal.fire({
+      title: `Hamburguesa ${item.nombre} quitado de carrito exitosamente`,
+      width: `450px`,
+      timer: 1500,
+      timerProgressBar: true,
+      backdrop: false,
+      showConfirmButton: false,
+    })
+    let cartaCarrito = document.getElementsByClassName(`${item.id}`);
+    
+    while (cartaCarrito.length > 0) {
+      cartaCarrito[0].remove();
+    }
+
+  }else {
+    console.log("no anda");
+  }
+}
+})
+//Boton "comprar" de carritoVacio
+  let botonComprar = document.createElement("div");
+  botonComprar.innerHTML = `
+  <div class="contenedorCarrito">
+  <button class="btn-emergente">Comprar</button>
+  </div>`;
+  
+  botonCarrito.append(botonComprar);
+  
+  botonCarrito.addEventListener("click", () => verCarrito);
+  
+  botonComprar.addEventListener("click", (e) => {
+    e.stopPropagation();
+    
+  botonComprar.addEventListener("click", botonComprarProducto)
+  })
+}
+  
+//Funcion para que el boton "comprar" del carrito ande
+function botonComprarProducto(){
+
+  let precioTotal = 0;
+
+  carritoVacio.forEach(item => {
+    precioTotal += item.precio;
+
+    Swal.fire({
+      title: `<p class="verde">Compra realizada exitosamente</p>`,
+      html: `Monto a pagar: <p class="verde">$${precioTotal}</p>`,
+      width: `450px`,
+      backdrop: false,
+    })
+     
+  })
+  console.log(precioTotal);
+}
+
+//Para abrir y cerrar el boton de productos (nuestras hamburguesas)
 function abrirCerrarBotonProductos(){
   if(verdad){
     verProductos();
@@ -67,174 +188,18 @@ function abrirCerrarBotonProductos(){
   }
 }
 
-botonMostrar.addEventListener("click", abrirCerrarBotonProductos);
-
-
-
-//boton comprar//
-
-
-
-function comprarProductos(){
-  let parrafo = document.createElement("p");
-  parrafo.innerHTML = `<p class="parrafo">Escriba el nombre de la hamburguesa que desea comprar</p>`;
-
-  let input = document.createElement("input");
-  input.className = "input";
-  input.setAttribute("placeholder","Ejemplo: doble cheddar")
-
-  let inputSubmit = document.createElement("input");
-  inputSubmit.type = "submit";
-  inputSubmit.className = "botonInput";
-  inputSubmit.value = "Agregar al carrito";
-
-
-
-
-  inputSubmit.addEventListener("click", () => {
-    const productoBuscado = input.value.toLowerCase(); //devuelve el valor escrito en el input
-    let contenedorJs;
-      switch (productoBuscado) {
-        case "simple":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa simple ha sido agregada</h2>
-            <p>$1000</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-
-            botonComprar.append(contenedorJs)
-          break;
-        case "con queso":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Con queso ha sido agregada</h2>
-            <p>$1200</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        case "triple queso":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Triple queso ha sido agregada</h2>
-            <p>$2000</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        case "big":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Big ha sido agregada</h2>
-            <p>$2100</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        case "triple tasty":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Triple tasty ha sido agregada</h2>
-            <p>$2300</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        case "doble bacon":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Doble bacon ha sido agregada</h2>
-            <p>$2100</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        case "doble cheddar":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Doble cheddar ha sido agregada</h2>
-            <p>$2500</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        case "triple cheddar":
-            contenedorJs = document.createElement("div");
-            contenedorJs.className = "contenedorJs";
-            contenedorJs.innerHTML = `
-            <h1>Bienvenido al carrito</h1>
-            <h2>Hamburguesa Triple cheddar ha sido agregada</h2>
-            <p>$3000</p>
-            <button type="text" class="btn-emergente">Comprar</button>
-            `
-            botonComprar.append(contenedorJs)
-            console.log("funciona");
-          break;
-        default:
-          alert("Escriba bien los nombres de los productos, gracias");
-          console.log("no funciona");
-
-          break;
-      }
-
-  })
-
-  inputSubmit.addEventListener("click", (evento) => { //para que no se cierre el botonComprar cuando hago click en buscar.
-    evento.stopPropagation();
-  })
-
-  input.addEventListener("click", (evento) => { //para que no se cierre el botonComprar cuando hago click en el input.
-    evento.stopPropagation();
-  })
-
-  input.addEventListener("keyup", (e) => { //para que al presionar y soltar el espacio desde la pc no se me cierre el botonComprar.
-    if(e.key === " "){
-      console.log("espacio");
-      e.preventDefault(); //evita el comportamiento x defecto que hace la tecla o los formularios o lo sea. en este caso la tecla "espacio".
-    }
-  });
-
-  botonComprar.append(parrafo);
-  botonComprar.append(input);
-  botonComprar.append(inputSubmit);
-}
-
-
-
-function abrirCerrarBotonComprar(){
+//para abrir y cerrar el boton de carrito
+function abrirCerrarBotonCarrito(){
   if(verdad){
-    console.log("abrir comprar");
-    comprarProductos();
+    console.log("abrir carrito");
+    verCarrito();
     verdad = false;
   }
   else {
-    console.log("cerrar comprar");
-    botonComprar.innerHTML = `<span class="boton">Comprar</span>`;
+    console.log("cerrar carrito");
+    botonCarrito.innerHTML = `<span class="boton">Carrito</span>`;
     verdad = true;
   }
 }
 
-botonComprar.addEventListener("click", abrirCerrarBotonComprar);
-
-
+botonCarrito.addEventListener("click", abrirCerrarBotonCarrito);
---
+++ js/sweetAlert.js
@@ -0,0 +1,63 @@
+/* swal.fire({
+    title: "Bienvenido",
+	text: "Esto es un texto",
+	// html: `<h1 class="ventana-alert">Elemento HTML reemplaza el "text" y puedo meterle css</h1>`, 
+	// icon: `success`,
+	confirmButtonText: `okey`,
+	// footer: `<span class="ventana-alert">pie de pagina</span>`,
+	// width: `300px`,
+	// padding: ``,
+	// background: ``,
+	// grow: `column`,
+	backdrop: false,             //fondo borroso
+	// timer: 8000,              //segundos antes de cerrarse
+	// timerProgressBar: true,   //barrito del tiempo
+	// toast: true,              //alerta chiquito
+	// position: `top`,
+	// allowOutsideClick: true,  //si queremos que el usuario de click afuera
+	// allowEscapeKey: ,         // si quiero permitir que con espace se cierre
+	// allowEnterKey: ,          // si quiero permitir que con enter se cierre
+	// stopKeydownPropagation: false,
+
+	// input: ``,
+	// inputPlaceholder: ``,
+	// inputValue: ``,
+	// inputOptions: ``,
+	
+	customClass: {
+
+        //  container: `ventana-alert`,
+        // 	popup: ``,
+        // 	header: ``,
+        // 	title: `nombre de la clase`,
+        // 	closeButton: ``,
+        // 	icon: ``,
+        //  image: `clase`,
+        //  content: ``,
+        // 	input: ``,
+        // 	actions: ``,
+        // 	confirmButton: ``,
+        // 	cancelButton: ``,
+        // 	footer:	 ``,
+
+    },
+
+	// showConfirmButton: true,
+	confirmButtonColor: `green`,
+	// confirmButtonAriaLabel: ``, //descripcion de que va el boton
+
+	// showCancelButton: ``,
+	// cancelButtonText: ``,
+	// cancelButtonColor: ``,
+	// cancelButtonAriaLabel: ``,
+	
+	// buttonsStyling: true,        //trabajar con estilos de la libreria o no
+	showCloseButton: true,
+	closeButtonAriaLabel: `cerrar`,
+
+
+	// imageUrl: `../imagenes/burgerroja.png`,
+	// imageWidth: ``,
+	// imageHeight: ``,
+	// imageAlt: ``,
+}) */
